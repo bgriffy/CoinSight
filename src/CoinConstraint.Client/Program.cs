404111763fa2214e70fe356833d193ced99c26a1
bgriffy <brentgriffin94@gmail.com>
2021-07-28 06:28:21 -0400

    Added datagrid - Added blazorise datagrid dependency for budget page - Added test datagrid code on index page

--- src/CoinConstraint.Client/Program.cs
+++ src/CoinConstraint.Client/Program.cs
@@ -1,11 +1,10 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CoinConstraint.Client
@@ -18,6 +17,14 @@ namespace CoinConstraint.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services
+              .AddBlazorise(options =>
+              {
+                  options.ChangeTextOnKeyPress = true;
+              })
+              .AddBootstrapProviders()
+              .AddFontAwesomeIcons();
 
             await builder.Build().RunAsync();
         }
