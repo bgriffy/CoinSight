53a743b61ff8c3813d30882479bfb78269cfad2c
bgriffy <brentgriffin94@gmail.com>
2022-01-23 19:12:10 -0500

    refactor: Add ExpenseDatagridComponent
    Working on moving index page expense grid to its own component.

---
+++ src/CoinConstraint.Client/Components/ExpenseDatagridComponent.razor.cs
@@ -0,0 +1,48 @@
+﻿using CoinConstraint.Domain.AggregateModels.BudgetingAggregate.Entities;
+
+namespace CoinConstraint.Client.Components;
+
+public partial class ExpenseDatagridComponent
+{
+    private bool _isSmallScreen;
+    private bool _isMediumScreen;
+    private bool _isLargeScreen;
+
+    [Parameter]
+    public Expense SelectedExpense { get; set; }
+
+    [Parameter]
+    public Budget SelectedBudget { get; set; }
+
+    [Parameter]
+    public EventCallback<Expense> ExpenseDeleted { get; set; }
+
+    [Parameter]
+    public EventCallback<Expense> ExpensePaymentRequested { get; set; }
+
+    [Parameter]
+    public EventCallback<Expense> ExpenseDetailRequested { get; set; }
+
+    [Parameter]
+    public EventCallback NewExpenseDetailRequested { get; set; }
+
+    private async Task HandleDeletedExpense(Expense expense)
+    {
+        await ExpenseDeleted.InvokeAsync(expense);
+    }
+
+    private async Task OpenPaymentURLAsync(Expense expense)
+    {
+        await ExpensePaymentRequested.InvokeAsync(expense);
+    }
+
+    private async Task OpenExpenseDetailModal(Expense expense)
+    {
+        await ExpenseDetailRequested.InvokeAsync(expense);
+    }
+
+    private async Task OpenNewExpenseDetailModal()
+    {
+        await NewExpenseDetailRequested.InvokeAsync();
+    }
+}
