8f9725a6a53a34e42684208fb41e7f68f5dc2ec4
bgriffy <brentgriffin94@gmail.com>
2022-02-24 22:49:09 -0500

    feat: Add scheduler logic to get Budget from schedule

--- src/CoinConstraint.BudgetScheduler/CoinConstraint.BudgetScheduler/BudgetScheduler.cs
+++ src/CoinConstraint.BudgetScheduler/CoinConstraint.BudgetScheduler/BudgetScheduler.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using CoinConstraint.Domain.AggregateModels.BudgetingAggregate.Entities;
 using System.Collections.Generic;
+using CoinConstraint.Domain.Enums;
 
 namespace CoinConstraint.BudgetScheduler;
 
@@ -20,34 +21,59 @@ public class BudgetScheduler
         log.LogInformation($"C# Timer trigger function  executed at: {DateTime.Now}");
 
         // Get the connection string from app settings and use it to create a connection.
-        //var connectionString = Environment.GetEnvironmentVariable("coinconstraint_connection");
-
+        var connectionString = Environment.GetEnvironmentVariable("coinconstraint_connection");
         var conn = new SqlConnection(connectionString);
         conn.Open();
 
+        var budgetSchedules = GetSchedules(conn);
+
+        foreach (var budgetSchedule in budgetSchedules)
+        {
+            var budget = GetBudgetFromSchedule(budgetSchedule, conn);
+        }
+    }
+
+    private static List<BudgetSchedule> GetSchedules(SqlConnection conn)
+    {
         var sql = "SELECT * FROM CoinConstraint.dbo.BudgetSchedules";
         var cmd = new SqlCommand(sql, conn);
         SqlDataReader rows = cmd.ExecuteReader();
 
-        var budgetSchedules = new List<BudgetSchedule>();   
-
+        var budgetSchedules = new List<BudgetSchedule>();
+         
         while (rows.Read())
         {
             var budgetSchedule = new BudgetSchedule()
             {
-                ID = (int)rows["id"], 
-                BudgetID = (int)rows["BudgetID"], 
-                EndDate = (DateTime)rows["EndDate"], 
-                StartDate = (DateTime)rows["StartDate"], 
-                LastScheduledDate = (DateTime)rows["LastScheduledDate"], 
-                NextScheduledDate = (DateTime)rows["NextScheduledDate"]
+                ID = (int)rows["id"],
+                BudgetID = (int)rows["BudgetID"],
+                EndDate = (DateTime)rows["EndDate"],
+                StartDate = (DateTime)rows["StartDate"],
+                LastScheduledDate = (DateTime)rows["LastScheduledDate"],
+                NextScheduledDate = (DateTime)rows["NextScheduledDate"],
+                ScheduleFrequency = (ScheduleFrequency)(int)rows["ScheduleFrequency"]
             };
 
-            budgetSchedules.Add(budgetSchedule);    
+            budgetSchedules.Add(budgetSchedule);
         }
 
-        Console.WriteLine(rows);
+        return budgetSchedules;
+    }
 
-        log.LogInformation($"{rows} rows were updated");
+    private static Budget GetBudgetFromSchedule(BudgetSchedule schedule, SqlConnection conn)
+    {
+        var budget = new Budget();
+        var sql = "SELECT TOP 1 * FROM CoinConstraint.dbo.Budgets WHERE BudgetID = @BudgetID";
+        var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters["@BudgetID"].Value = schedule.BudgetID;
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            budget.ID= (int?)reader["id"];
+        }
+
+        return budget;
+
     }
 }
